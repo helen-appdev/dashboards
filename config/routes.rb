c5d6e30057c38c2bfdb0b96a11bc5d57ea7a8e9b
helenavocado <hwang92@uchicago.edu>
2021-10-30 02:23:37 +0000

    in one go

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", {:controller => "forex", :action => "home"})
+  get("/forex", { :controller => "forex", :action => "forexlist"})
+  get("/forex/:fromc1", { :controller => "forex", :action => "convertlist"})
+  get("/forex/:fromc1/:toc2", { :controller => "forex", :action => "convertaction"})
+  
+
+
 end
